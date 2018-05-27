dbc50e7689f708c6f5858b3b097a28ee5f0b5236
Danielhui <1106233055hui@gmail.com>
2018-05-27 13:52:14 +0800

    1-3
    下拉刷新

--- cn-wechat-weather/pages/index/index.js
+++ cn-wechat-weather/pages/index/index.js
@@ -7,12 +7,31 @@ const weatherMap = {
   'snow': '雪'
 }
 
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+
 Page({
   data:{
     nowTemp:'14',
     nowWeather:"多云",
+    nowWeatherBackground:""
+  },
+  onPullDownRefresh(){
+    this.getNow(() =>{
+      wx.stopPullDownRefresh()
+    })
+    console.log("refresh executed")
   },
   onLoad(){
+    this.getNow()
+  },
+  getNow(callback){
     console.log("hello everyone")
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
@@ -24,9 +43,17 @@ Page({
         let weather = result.now.weather
         console.log(temp, weather)
         this.setData({
-          nowTemp:temp,
-          nowWeather:weatherMap[weather],
+          nowTemp: temp,
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png',
         })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      },
+      complete:() => {
+        callback && callback()
       }
     })
   }
