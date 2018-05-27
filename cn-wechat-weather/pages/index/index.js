955a31f6a5668972f58ad286d42803858ff502ee
Danielhui <1106233055hui@gmail.com>
2018-05-27 21:24:08 +0800

    1-6
    second page

--- cn-wechat-weather/pages/index/index.js
+++ cn-wechat-weather/pages/index/index.js
@@ -18,10 +18,12 @@ const weatherColorMap = {
 
 Page({
   data:{
-    nowTemp:'14',
-    nowWeather:"多云",
+    nowTemp:'',
+    nowWeather:"",
     nowWeatherBackground:"",
-    forecast:[]
+    hourlyWeather:[],
+    todayTemp:"",
+    todayDate:"",
   },
   onPullDownRefresh(){
     this.getNow(() =>{
@@ -40,38 +42,60 @@ Page({
       success: res => {
         console.log(res)
         let result = res.data.result
-        let temp = result.now.temp
-        let weather = result.now.weather
-        console.log(temp, weather)
-        this.setData({
-          nowTemp: temp,
-          nowWeather: weatherMap[weather],
-          nowWeatherBackground: '/images/' + weather + '-bg.png',
-        })
-        wx.setNavigationBarColor({
-          frontColor: '#000000',
-          backgroundColor: weatherColorMap[weather],
-        })
-
-        let nowHour = new Date().getHours()
-        console.log(nowHour)
-        let forecast = []
-        for (let i = 0; i<24;i += 3){
-          forecast.push({
-            time: (i+ nowHour)%24 + '时',
-            iconPath: '/images/sunny-icon.png',
-            temp:"12*"
-          })
-        }
-        forecast[0].time = '现在'
-        this.setData({
-          forecast:forecast,
-        })
+        this.setNow (result)
+        this.setHourlyWeather(result)
+        this.setToday(result)
       },
 
       complete:() => {
         callback && callback()
       }
+    })
+  },
+  setNow(result){
+    let temp = result.now.temp
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp,
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png',
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+    })
+  },
+
+  setHourlyWeather(result){
+    let nowHour = new Date().getHours()
+    let forecast = result.forecast
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i += 1) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + '时',
+        iconPath: '/images/' + forecast[i].weather + '-icon.png',
+        temp: forecast[i].temp + '°'
+      })
+    }
+    hourlyWeather[0].time = '现在'
+    this.setData({
+      hourlyWeather: hourlyWeather,
+    })
+  },
+
+setToday(result){
+  let date = new Date()
+  this.setData({
+    todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+    todayDate: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()} 今天`
+    })
+  },
+
+  onTapDayWeather() {
+    wx.showToast()
+    wx.navigateTo({
+      url: '/pages/list/list',
     })
   }
 })
