cf80d2f477d3113115887a0b72c221d6372822a6
Danielhui <1106233055hui@gmail.com>
2018-05-27 12:38:37 +0800

    1-2
    从api中获取数据

--- cn-wechat-weather/pages/index/index.js
+++ cn-wechat-weather/pages/index/index.js
@@ -1,5 +1,20 @@
 Page({
+  data:{
+    nowTemp:14,
+    nowWeather:"多云"
+  },
   onLoad(){
-    console.log("hello everyday")
+    console.log("hello everyone")
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: { "city": "南昌市" },
+      success: res => {
+        console.log(res)
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+      }
+    })
   }
 })
